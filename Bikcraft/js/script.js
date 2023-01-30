523d222c68f54d420ed6975582d99cd6c9dce6e6
catelmatt <catelmatt@hotmail.com>
2023-01-30 14:18:44 +0044

    Menu Links Activation

---
+++ Bikcraft/js/script.js
@@ -0,0 +1,14 @@
+//Menu Link Activation
+
+const links = document.querySelectorAll(".header-menu a");
+
+function linkActivation(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("active");
+  }
+}
+
+links.forEach(linkActivation);
