78ebb49b1cb71f47c89d404d0c26835b7bc45e95
catelmatt <catelmatt@hotmail.com>
2023-01-30 15:08:35 +0044

    FAQ Box Expand

--- Bikcraft/js/script.js
+++ Bikcraft/js/script.js
@@ -12,3 +12,23 @@ function linkActivation(link) {
 }
 
 links.forEach(linkActivation);
+
+// Frequently Asked Questions Box Expand
+
+const buttonExpand = document.querySelectorAll(".faq button");
+
+function activeExpand(e) {
+  const question = e.currentTarget;
+  const controls = question.getAttribute("aria-controls");
+  const response = document.getElementById(controls);
+
+  response.classList.toggle("active");
+  const active = question.classList.contains("active");
+  question.setAtribute("aria-expanded", active);
+}
+
+function expandEvent(question) {
+  question.addEventListener("click", activeExpand);
+}
+
+buttonExpand.forEach(expandEvent);
